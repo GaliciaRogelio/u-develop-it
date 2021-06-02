630ddc37e894eee65ba7cdcb645ecd13fd0e5804
Rogelio <rogeliogalicia@Rogelios-Air.attlocal.net>
2021-06-01 17:37:18 -0700

    update candidate routes to join party data

--- server.js
+++ server.js
@@ -23,7 +23,11 @@ const db = mysql.createConnection(
 
 // Get all candidates
 app.get("/api/candidates", (req, res) => {
-  const sql = `SELECT * FROM candidates`;
+  const sql = `SELECT candidates.*, parties.name
+              AS party_name
+              FROM candidates
+              LEFT JOIN parties
+              ON candidates.party_id = parties.id`;
 
   db.query(sql, (err, rows) => {
     if (err) {
@@ -38,21 +42,27 @@ app.get("/api/candidates", (req, res) => {
 });
 
 // Get a single candidate
-// app.get("/api/candidate/:id", (req, res) => {
-//   const sql = `SELECT * FROM candidates WHERE id = ?`;
-//   const params = [req.params.id];
+app.get("/api/candidate/:id", (req, res) => {
+  const sql = `SELECT candidates.*, parties.name
+              AS party_name
+              FROM candidates
+              LEFT JOIN parties
+              ON candidates.party_id = parties.id
+              WHERE candidates.id = ?`;
 
-//   db.query(sql, params, (err, row) => {
-//     if (err) {
-//       res.status(400).json({ error: err.message });
-//       return;
-//     }
-//     res.json({
-//       message: "Success",
-//       data: row,
-//     });
-//   });
-// });
+  const params = [req.params.id];
+
+  db.query(sql, params, (err, row) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "Success",
+      data: row,
+    });
+  });
+});
 
 // Delete candidate
 // app.delete("/api/candidate/:id", (req, res) => {
